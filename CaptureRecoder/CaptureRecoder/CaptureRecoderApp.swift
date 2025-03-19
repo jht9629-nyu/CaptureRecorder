aace06912879f91f2090f8e3ba653b668c6ae890
John Henry Thompson <78864877+jht9629-nyu@users.noreply.github.com>
2025-03-19 12:59:11 -0400

    CaptureRecoder/CaptureRecoder

---
+++ CaptureRecoder/CaptureRecoder/CaptureRecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaptureRecoderApp.swift
+//  CaptureRecoder
+//
+//  Created by jht2 on 3/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CaptureRecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CaptureRecoder/CaptureRecoder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CaptureRecoder
+//
+//  Created by jht2 on 3/19/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
